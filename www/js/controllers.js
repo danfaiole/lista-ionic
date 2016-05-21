bc928bc800970a0a768105c1949053e137157931
Daniel Faiole <danfaiole@gmail.com>
2016-05-21 16:53:56 -0300

    insert contatos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,22 @@
 angular.module('app.controllers', [])
-  
+
 .controller('contactDetailsCtrl', function($scope) {
 
 })
-   
-.controller('newContactCtrl', function($scope) {
 
+.controller('newContactCtrl', function($scope) {
+  $scope.createContact = function(contact){
+    var query = "insert into contato(name, email, phone) values ('"+contact.name+"', '"+contact.email+"', '"+contact.phone+"')";
+    //$scope.contact['name'] = '';
+    //console.log(query)
+    $cordovaSQLite.execute(db, query, []).then(function(res) {
+        console.log("INSERT ID -> " + res.insertId);
+    }, function (err) {
+        console.error(err);
+    });
+  }
 })
-      
+
 .controller('contactsCtrl', function($scope) {
 
 })
- 
