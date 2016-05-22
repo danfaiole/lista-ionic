d58834c021c222f10d1573a62e2dcf4e3088b81d
Daniel Faiole <danfaiole@gmail.com>
2016-05-22 15:00:09 -0300

    query select contatos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,10 @@
 angular.module('app.controllers', [])
-  
+
 .controller('contactDetailsCtrl', function($scope, $cordovaSQLite) {
 
 
 })
-   
+
 .controller('newContactCtrl', function($scope, $cordovaSQLite) {
     $scope.name = "";
     $scope.email = "";
@@ -18,7 +18,16 @@ angular.module('app.controllers', [])
     })
   }
 })
-      
-.controller('contactsCtrl', function($scope) {
 
+.controller('contactsCtrl', function($scope, $cordovaSQLite) {
+  $scope.contacts = []
+  $cordovaSQLite.execute(db, 'select name from contatos', []).then(function(result) {
+    if (result.rows.length > 0) {
+      for(var i = 0; i < result.rows.length; i++) {
+        $scope.contacts.push(
+          {name: resuls.rows.item(i).name}
+        )
+      }
+    }
+  })
 })
