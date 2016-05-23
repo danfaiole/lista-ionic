367663481bb5ec833660f427e5d922709bb64d5e
Daniel Faiole <danfaiole@gmail.com>
2016-05-23 12:40:18 -0300

    finished the edit function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,18 @@
 angular.module('app.controllers', [])
 
-.controller('contactDetailsCtrl', function ($scope, $stateParams) {
-    $scope.id =  contacts[$stateParams.id].id
-    $scope.name = contacts[$stateParams.id].name
-    $scope.email = contacts[$stateParams.id].email
-    $scope.phone = contacts[$stateParams.id].phone
+.controller('contactDetailsCtrl', function($scope, $stateParams ) {
+  $scope.name = contacts[$stateParams.id].name
+  $scope.email = contacts[$stateParams.id].email
+  $scope.phone = contacts[$stateParams.id].phone
+  $scope.id = contacts[$stateParams.id].id
 })
 
-    .controller('newContactCtrl', function($scope) {
+.controller('newContactCtrl', function($scope) {
     $scope.name = "";
     $scope.email = "";
     $scope.phone = "";
     $scope.insert = function() {
-      id = contacts.length;
+      id = contacts.length
       contacts.push({name: this.name, email: this.email, phone: this.phone, id: id})
 
       this.name = "";
@@ -21,18 +21,18 @@ angular.module('app.controllers', [])
   }
 })
 
-.controller('contactsCtrl', function ($scope) {
-    $scope.contacts = contacts
+.controller('contactsCtrl', function($scope) {
+  $scope.contacts = contacts
 })
 
-.controller('contactEditCtrl', function ($scope, $stateParams) {
-    $scope.insert = function () {
-        contacts[$stateParams.id].name = this.name;
-        contacts[$stateParams.id].email = this.email;
-        contacts[$stateParams.id].phone = this.phone;
-    }
+.controller('contactEditCtrl', function($scope, $stateParams) {
+  $scope.name = contacts[$stateParams.id].name
+  $scope.email = contacts[$stateParams.id].email
+  $scope.phone = contacts[$stateParams.id].phone
 
-    this.name = "";
-    this.email = "";
-    this.phone = "";
+  $scope.update = function(){
+    contacts[$stateParams.id].name = this.name
+    contacts[$stateParams.id].email = this.email
+    contacts[$stateParams.id].phone = this.phone
+  }
 })
